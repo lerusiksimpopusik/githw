9c6b92c13067b2e44e6a2fcadb7ac94204cc124d
Valeria Chernodolya <chernodolja@gmail.com>
2025-03-20 07:44:40 +0700

    tests for is_red

--- tests.cpp
+++ tests.cpp
@@ -73,3 +73,20 @@ TEST(CandleTest, BodySize_LargeBody) {
     Candle candle(50, 200, 20, 150);
     EXPECT_EQ(candle.body_size(), 100);  
 }
+
+// Тесты для метода is_red
+TEST(CandleTest, IsRed_True) {
+    Candle candle(110, 100, 90, 105);
+    EXPECT_TRUE(candle.is_red());
+}
+
+TEST(CandleTest, IsRed_False) {
+    Candle candle(100, 110, 90, 105);
+    EXPECT_FALSE(candle.is_red());
+}
+
+TEST(CandleTest, IsRed_NoBody) {
+    Candle candle(100, 100, 90, 105);
+    EXPECT_FALSE(candle.is_red());
+}
+
